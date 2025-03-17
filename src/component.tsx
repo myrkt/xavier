fdaf899fa9abca589cc2eb748ce1cdd97ce4ea56
Joshua Augustin <josh@tempo.fit>
2025-03-17 11:50:49 -0700

    Uses localStorage by default for caching experiment results

--- src/component.tsx
+++ src/component.tsx
@@ -1,5 +1,6 @@
 import React, { createContext, ReactNode } from "react";
 import { XavierApplication } from "./xavier";
+import { SWRConfig } from "swr";
 
 export const XavierContext = createContext<XavierApplication | null>(null);
 
@@ -9,14 +10,37 @@ interface XavierProviderProps {
   children: ReactNode;
 }
 
+function localStorageProvider(key: string): Map<string, any> {
+  // When initializing, we restore the data from `localStorage` into a map.
+  const map = new Map<string, any>(
+    JSON.parse(localStorage.getItem(key) || "[]"),
+  );
+
+  // Before unloading the app, we write back all the data into `localStorage`.
+  window.addEventListener("beforeunload", () => {
+    const appCache = JSON.stringify(Array.from(map.entries()));
+    localStorage.setItem(key, appCache);
+  });
+
+  // We still use the map for write & read for performance.
+  return map;
+}
+
 export const XavierProvider: React.FC<XavierProviderProps> = ({
   applicationId,
   apiToken,
   children,
 }) => {
   const instance = new XavierApplication(applicationId, apiToken);
+  const localStorageKey = `xavier-${applicationId}`;
 
   return (
-    <XavierContext.Provider value={instance}>{children}</XavierContext.Provider>
+    <XavierContext.Provider value={instance}>
+      <SWRConfig
+        value={{ provider: () => localStorageProvider(localStorageKey) }}
+      >
+        {children}
+      </SWRConfig>
+    </XavierContext.Provider>
   );
 };
